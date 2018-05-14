807d70ed3201df56ff2f06d371a90bfe7f2431c6
Steven C <stevenc.github@gmail.com>
2018-05-14 13:49:40 -0400

    Clean Up, Generic file path

--- MOVE_Project_Dir.bat
+++ MOVE_Project_Dir.bat
@@ -1,5 +1,5 @@
 :: this script will MOVE a folder, all files and sub-folders contained within. 
-:: it is executed at the end of the "Gorilla-Project-Zip" workflow.
+:: it is executed at the end of the "Vantage-Zip" workflow.
 :: after a project has been sucessfully archived, vantage will pass the path of the source folder into this script. 
 
-MOVE /Y %1 V:\NGCI\WatchFolders\Gorilla-Project-CheckIn\Zip-Archive-Output
+MOVE /Y %1 X:\file-path-to-Zip-CheckIn\Zip-Archive-Output
